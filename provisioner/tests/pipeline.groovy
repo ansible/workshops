ac0cbc0bce90c1d35a01be1a7575a7a8d5f6c70e
Yanis Guenane <yanis@guenane.org>
2019-08-15 23:21:37 +0200

    Tower: Bump to version 3.5.2

--- provisioner/tests/pipeline.groovy
+++ provisioner/tests/pipeline.groovy
@@ -6,7 +6,7 @@ pipeline {
         choice(
             name: 'TOWER_VERSION',
             description: 'Tower version to deploy',
-            choices: ['devel', '3.5.1']
+            choices: ['devel', '3.5.2']
         )
         choice(
             name: 'ANSIBLE_VERSION',

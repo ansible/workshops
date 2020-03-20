662013ca6c251220b0b72b12737844427403559b
Yanis Guenane <yanis@guenane.org>
2020-03-20 15:15:27 +0100

    Fix git clone workshop

--- provisioner/tests/gating.groovy
+++ provisioner/tests/gating.groovy
@@ -48,6 +48,7 @@ Build Tag: ${env.BUILD_TAG}"""
 tower_installer_url: https://releases.ansible.com/ansible-tower/setup/ansible-tower-setup-${TOWER_VERSION}-1.tar.gz
 admin_password: ${ADMIN_PASSWORD}
 ansible_workshops_refspec: ${ANSIBLE_WORKSHOPS_REFSPEC}
+ansible_workshops_version: ${env.BRANCH_NAME}
 ec2_name_prefix: tqe-{{ workshop_type }}-tower${DOTLESS_TOWER_VERSION}-${env.BRANCH_NAME}-${env.BUILD_ID}
 EOF
 """
